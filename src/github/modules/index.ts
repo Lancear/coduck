cd5c5e66392f966878a0dc7aecd662f4e6831efd
Lance J <lancear153@gmail.com>
2023-03-07 08:28:07 +0100

    Fix github imports

--- src/github/modules/index.ts
+++ src/github/modules/index.ts
@@ -1,5 +1,5 @@
-import { GithubPullRequest } from "../domain";
-import { fetchFromGithub, getResponseData } from "../internal/fetch";
+import { GithubPullRequest } from "../domain.js";
+import { fetchFromGithub, getResponseData } from "../internal/fetch.js";
 
 export async function fetchPullRequest(owner: string, repository: string, number: number): Promise<GithubPullRequest> {
   const response = await fetchFromGithub('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
